be3fb597a1871c3959b9383182b2434ffbd3d8e4
Darko Grozdanovski <dgrozdanovski@mobileiron.com>
2018-06-06 16:20:05 +0200

    adding sample screens

---
+++ app/src/main/java/com/darkog/android/examples/emmconfig/LoginActivity.kt
@@ -0,0 +1,51 @@
+package com.darkog.android.examples.emmconfig
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import io.reactivex.*
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_login.*
+import java.util.concurrent.TimeUnit
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        buttonSignIn.setOnClickListener { signIn() }
+    }
+
+    private fun signIn() {
+        Single.just(true)
+                .compose(applyProgressTransformer())
+                .observeOn(Schedulers.io())
+                .delay(1, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ _ -> navigateToMainActivity() })
+    }
+
+    private fun navigateToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun showProgress(show: Boolean) {
+        progressBar.visibility = if (show) View.VISIBLE else View.GONE
+        loginForm.visibility = if (show) View.GONE else View.VISIBLE
+    }
+
+    private fun <T> applyProgressTransformer(): SingleTransformer<T, T> {
+        return SingleTransformer {
+            it.observeOn(AndroidSchedulers.mainThread())
+                    .doOnError { showProgress(false) }
+                    .doOnSubscribe { showProgress(true) }
+        }
+
+    }
+}
--- app/src/main/java/com/darkog/android/examples/emmconfig/MainActivity.kt
+++ app/src/main/java/com/darkog/android/examples/emmconfig/MainActivity.kt
@@ -1,12 +1,56 @@
 package com.darkog.android.examples.emmconfig
 
-import android.support.v7.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.webkit.*
+import android.widget.Toast
+import com.darkog.android.examples.emmconfig.R.layout.activity_main
+import io.reactivex.Single
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
+        buttonSearch.setOnClickListener { loadUrl(editTextSearch.text.toString()) }
+        configureWebView()
+    }
+
+    private fun loadUrl(url: String) {
+        Single.just(url)
+                .map { it.normalizeUrl() }
+                .subscribe({ item ->
+                    editTextSearch.setText(item)
+                    webView.loadUrl(item)
+                }, { throwable ->
+                    Toast.makeText(this, getString(R.string.toast_message_invalid_url), Toast.LENGTH_LONG).show()
+                })
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun configureWebView() {
+        webView.settings.javaScriptEnabled = true
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+            }
+        }
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String?) {
+                super.onPageFinished(view, url)
+            }
+
+            override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onReceivedError(view: WebView, request: WebResourceRequest?, error: WebResourceError?) {
+                super.onReceivedError(view, request, error)
+            }
+        }
     }
 }
