96583fe9c98dea4f340d0cec995a1e67ece92047
Darko Grozdanovski <dgrozdanovski@mobileiron.com>
2018-06-23 14:12:44 +0200

    finishing test version code

--- app/src/main/java/com/darkog/android/examples/emmconfig/AppManagedConfigurationManager.kt
+++ app/src/main/java/com/darkog/android/examples/emmconfig/AppManagedConfigurationManager.kt
@@ -1,14 +1,70 @@
 package com.darkog.android.examples.emmconfig
 
+import android.content.Context
+import android.content.RestrictionEntry
+import android.content.RestrictionsManager
 import android.os.Build
+import android.os.Bundle
 
 
+class AppManagedConfigurationManager(context: Context) {
 
-class AppManagedConfigurationManager {
+    private val context: Context = context.applicationContext
+    private val restrictionsManager = context.getSystemService(Context.RESTRICTIONS_SERVICE) as RestrictionsManager
+    private lateinit var entriesMap: Map<String, RestrictionEntry>
+    private lateinit var applicationRestrictions: Bundle
 
-    private val BUNDLE_SUPPORTED = Build.VERSION.SDK_INT >= 23
+    init {
+        updateValues()
+    }
 
-    private val KEY_HOMEPAGE_URL = "homepageUrl"
-    private val KEY_HTTPS_ONLY = "useHttpsOnly"
+    private fun updateValues() {
+        entriesMap = restrictionsManager.getManifestRestrictions(context.packageName).associateBy { it.key }
+        applicationRestrictions = restrictionsManager.applicationRestrictions
+    }
 
+    fun canIncrementNumber() = resolveBoolean(KEY_CAN_INCREMENT_NUMBER)
+    fun number() = resolveInt(KEY_NUMBER)
+    fun welcomeMessage() = resolveString(KEY_WELCOME_MESSAGE)
+    fun color() = resolveString(KEY_COLORS)
+    fun userLevel() = resolveMulti(KEY_USER_ROLE)
+    fun secretCode() = resolveString(KEY_SECRET_CODE)
+    fun homepageUrl() = resolveString(KEY_HOMEPAGE_URL)
+
+
+    private fun resolveMulti(key: String) = resolve(key, { it.getStringArrayList(key) }, { it.allSelectedStrings.toList() })
+    private fun resolveString(key: String) = resolve(key, { it.getString(key) }, { it.selectedString })
+    private fun resolveInt(key: String) = resolve(key, { it.getInt(key) }, { it.intValue })
+    private fun resolveBoolean(key: String) = resolve(key, { it.getBoolean(key) }, { it.selectedState })
+
+    private fun <T> resolve(key: String, fromBundle: (bundle: Bundle) -> T, fromEntries: (entry: RestrictionEntry) -> T): T {
+        return if (applicationRestrictions.containsKey(key)) {
+            applicationRestrictions.get(key) as T
+        } else {
+            fromEntries(entriesMap.getValue(key))
+        }
+    }
+
+    companion object {
+        private val BUNDLE_SUPPORTED = Build.VERSION.SDK_INT >= 23
+
+        private const val KEY_CAN_INCREMENT_NUMBER = "can_increment_number"
+        private const val KEY_WELCOME_MESSAGE = "welcome_message"
+        private const val KEY_NUMBER = "number"
+        private const val KEY_COLORS = "colors"
+        private const val KEY_USER_ROLE = "user_role"
+        private const val KEY_SECRET_CODE = "secret_code"
+        private const val KEY_BOOKMARK_LIST = "bookmark_list"
+        private const val KEY_BOOKMARK = "bookmark"
+        private const val KEY_BOOKMARK_NAME = "bookmark_name"
+        private const val KEY_BOOKMARK_VALUE = "bookmark_value"
+
+        private const val KEY_HOMEPAGE_URL = "homepageUrl"
+        private const val KEY_HTTPS_ONLY = "useHttpsOnly"
+
+        fun isBundleSupported(): Boolean {
+            return BUNDLE_SUPPORTED
+        }
+
+    }
 }
--- app/src/main/java/com/darkog/android/examples/emmconfig/SimpleActivity.kt
+++ app/src/main/java/com/darkog/android/examples/emmconfig/SimpleActivity.kt
@@ -1,12 +1,55 @@
 package com.darkog.android.examples.emmconfig
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import com.darkog.android.examples.emmconfig.R.layout.activity_simple
+import kotlinx.android.synthetic.main.activity_simple.*
 
 class SimpleActivity : AppCompatActivity() {
 
+    private lateinit var appManagedConfigurationManager: AppManagedConfigurationManager
+    private lateinit var counterModifier: CounterModifier
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_simple)
+        setContentView(activity_simple)
+
+        appManagedConfigurationManager = AppManagedConfigurationManager(this)
+
+        counterModifier = CounterModifier(buttonIncrement)
+        updateConfiguredValues()
+    }
+
+    private fun updateConfiguredValues() {
+        buttonIncrement.isEnabled = appManagedConfigurationManager.canIncrementNumber()
+        counterModifier.counter = appManagedConfigurationManager.number()
+
+        textViewWelcomeMessage.text = appManagedConfigurationManager.welcomeMessage()
+        textViewColorSelection.text = appManagedConfigurationManager.color()
+        textViewUserLevel.text = TextUtils.join("\n", appManagedConfigurationManager.userLevel())
+        textViewSecretCode.text = getString(R.string.text_secret_code, appManagedConfigurationManager.secretCode())
+    }
+
+    private class CounterModifier(val button: Button) {
+        var counter: Int = 0
+            set(value) {
+                field = value
+                updateCount()
+            }
+
+        init {
+            button.setOnClickListener {
+                counter++
+            }
+            updateCount()
+        }
+
+        @SuppressLint("StringFormatMatches")
+        private fun updateCount() {
+            button.text = button.context.getString(R.string.counter_value, this.counter)
+        }
     }
 }
